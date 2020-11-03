0a962a62df091fcc3c505b49fbd4b9c552e8d9d4
Xiong-yp <1653618612@qq.com>
2020-11-03 14:54:58 +0800

    1

---
+++ Assets/Scripts/Controani.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Controani : MonoBehaviour
+{
+    Animator _ani;
+    NavMeshAgent _agen;
+    NavMeshObstacle _obstacle;
+    float max_x;
+    float min_x;
+    float max_z;
+    float min_z;
+    Vector3 _target;
+    float tis;
+    float dis;
+    private void Awake()
+    {
+        _ani = transform.GetComponent<Animator>();
+        _agen = transform.GetComponent<NavMeshAgent>();
+        _obstacle = transform.GetComponent<NavMeshObstacle>();
+        max_x = transform.localPosition.x + 10;
+        min_x = transform.localPosition.x - 10;
+        max_z = transform.localPosition.z + 10;
+        min_z = transform.localPosition.z - 10;
+    }
+    void Pos()
+    {
+        _target = new Vector3(Random.Range(min_x, max_x), 0, Random.Range(min_z, max_z));
+    }
+    void isagent()
+    {
+        _agen.enabled = true;
+        _agen.destination = _target;
+    }
+    void Arrive()
+    {
+        tis = dis;
+        dis = Vector3.Distance(transform.localPosition, _target);
+        if (tis != dis)
+        {
+            if (!_agen.isActiveAndEnabled && _obstacle.isActiveAndEnabled)
+            {
+                _obstacle.enabled = false;
+                Invoke("isagent", 0.1f);
+            }
+            _ani.SetBool("grooming", false);
+            _ani.SetBool("walk", true);
+        }
+        if (_agen.pathStatus == NavMeshPathStatus.PathComplete && !_agen.hasPath && !_agen.pathPending && _agen.remainingDistance < 1f)
+        {
+            if (_agen.isActiveAndEnabled)
+            {
+                _agen.enabled = false;
+                _obstacle.enabled = true;
+            }
+
+        }
+        //else
+        //{
+        //    //_ani.SetBool("grooming", false);
+        //    //_ani.SetBool("walk", true);
+        //}
+        if (_agen.hasPath && _agen.remainingDistance < 1f)
+        {
+            if (_agen.isActiveAndEnabled)
+            {
+                _agen.enabled = false;
+                _obstacle.enabled = true;
+            }
+            _ani.SetBool("grooming", true);
+            _ani.SetBool("walk", false);
+
+        }
+    }
+    private void Start()
+    {
+        InvokeRepeating("Pos", 0, 10f);
+    }
+    void Update()
+    {
+        Arrive();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    NavMeshAgent _agent;
+    Ray _ray;
+    RaycastHit _hit;
+    int _floorMask;
+    float _camLength = 100f;
+    Vector3 _moveTarget;
+    Animator _ani;
+    NavMeshObstacle _obstacle;
+    float _dis;
+    float _tis;
+    private void Awake()
+    {
+        _agent = transform.GetComponent<NavMeshAgent>();
+        _floorMask = LayerMask.GetMask("Floor");
+        _ani = transform.GetComponent<Animator>();
+        _obstacle = transform.GetComponent<NavMeshObstacle>();
+    }
+    void isagent()
+    {
+        _agent.enabled = true;
+        if (Physics.Raycast(_ray, out _hit, _camLength, _floorMask))
+        {
+            _moveTarget = _hit.point;
+            _agent.destination = _moveTarget;
+        }
+    }
+    void PlayerMove()
+    {
+        _tis = _dis;
+        _dis = Vector3.Distance(transform.localPosition, _moveTarget);
+        if (_tis != _dis)
+        {
+            if (_obstacle.isActiveAndEnabled)
+            {
+                _obstacle.enabled = false;
+                Invoke("isagent", 0.1f);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(_ray, out _hit, _camLength, _floorMask))
+            {
+                _moveTarget = _hit.point;
+                _agent.destination = _moveTarget;
+                _ani.SetBool("idie", false);
+                _ani.SetBool("run", true);
+            }
+        }
+        if (_agent.pathStatus == NavMeshPathStatus.PathComplete && !_agent.hasPath && !_agent.pathPending && _agent.remainingDistance < 1f)
+        {
+            _ani.SetBool("idie", true);
+            _ani.SetBool("run", false);
+        }
+        if (_agent.hasPath && _agent.remainingDistance < 1f)
+        {
+            if (_agent.isActiveAndEnabled)
+            {
+                _agent.enabled = false;
+                _obstacle.enabled = true;
+            }
+            _ani.SetBool("idie", true);
+            _ani.SetBool("run", false);
+        }
+    }
+    void Update()
+    {
+        PlayerMove();
+    }
+}
