23f529454b5232f6163b17c72643ef0b64243351
ETyoshino <576534340@qq.com>
2020-11-09 20:49:02 +0800

    lx

---
+++ Assets/Scenes/BuildTest/HoseTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoseTrigger : MonoBehaviour
+{
+    private Material _material;
+    private Color _texture;
+    
+    private void Awake()
+    {
+        _material = GetComponentInChildren<MeshRenderer>().material;
+        _texture = _material.GetColor("_Color");
+        Debug.Log(_texture);
+        _texture = new Color(102, 214, 80, 146)/255;
+        _material.SetColor("_Color",_texture);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+        if (other.gameObject.tag == "House")
+        {
+            Debug.Log("OnTriggerEnter");
+            _texture = new Color(229,91,55,146)/255;
+            _material.SetColor("_Color",_texture);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit");
+        _texture = new Color(102, 214, 80, 146)/255;
+        _material.SetColor("_Color",_texture);
+    }
+}
