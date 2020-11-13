e8e34ce509bcc36bc0e7945b41f117d2e29e4cb7
Xiong-yp <1653618612@qq.com>
2020-11-13 18:10:14 +0800

    7

--- Assets/Scripts/Mgr/ConfigMgr.cs
+++ Assets/Scripts/Mgr/ConfigMgr.cs
@@ -83,8 +83,8 @@ public class ConfigMgr : MonoBehaviour
                 }
             }
             _tempnpc = Instantiate(Resources.Load(string.Format("{0}{1}", "Npc/", _modle)) as GameObject);
-            _tempnpc.AddComponent<NPC>().Inst(_id, _name, _modle, _professional, _housename, _tradable, _startwork, _endwork, _speack);
             _tempnpc.SetActive(false);
+            _tempnpc.AddComponent<NPC>().Inst(_id, _name, _modle, _professional, _housename, _tradable, _startwork, _endwork, _speack);
 
             for (int x = 0; x < GameMgr.Instance.HourseMgr.Temphourse.Count; x++)
             {
