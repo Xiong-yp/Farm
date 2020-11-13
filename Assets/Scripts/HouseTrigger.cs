acc7fed02cea3109aa78ff1fb2292b1b6205a5da
ETyoshino <576534340@qq.com>
2020-11-13 19:41:25 +0800

    1

--- Assets/Scripts/HouseTrigger.cs
+++ Assets/Scripts/HouseTrigger.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 
 public class HouseTrigger : MonoBehaviour
 {
+    
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Here cant Build Struct");
+        GameMgr.Instance.BuildMgr.ChangePlaneColor(false);
     }
 
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("Here can Build Struct");
+        GameMgr.Instance.BuildMgr.ChangePlaneColor(true);
     }
 }
