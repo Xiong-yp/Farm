695df40ba117234ffc9fb554bf756a37e99b29f7
ETyoshino <576534340@qq.com>
2020-11-14 15:03:56 +0800

    1

--- Assets/Scripts/Mgr/GameMgr.cs
+++ Assets/Scripts/Mgr/GameMgr.cs
@@ -49,8 +49,6 @@ public class GameMgr : MonoBehaviour
     {
         _instance = this;
         Init();
-<<<<<<< HEAD
-        
     }
 
     public CameraMgr CameraMgr
@@ -58,35 +56,23 @@ public class GameMgr : MonoBehaviour
         get;
         private set;
     }
-    
+
     void Init()
     {
         mainCamera = Camera.main;
         CameraMgr = gameObject.AddComponent<CameraMgr>();
-        
-=======
->>>>>>> 796e79390ecfda500864993fe55a5a4427d444d4
+
+
         TimeMgr = gameObject.AddComponent<TimeMgr>();
         TimeMgr.Inst();
         HourseMgr = gameObject.AddComponent<HourseMgr>();
         HourseMgr.Inst();
         ConfigMgr = gameObject.AddComponent<ConfigMgr>();
         ConfigMgr.Inst();
-<<<<<<< HEAD
+
         BuildMgr = gameObject.AddComponent<BuildMgr>();
         //BuildMgr.Init();
         UIMgr = gameObject.AddComponent<UIMgr>();
         //UIMgr.Init();
-=======
-        //BuildMgr = gameObject.AddComponent<BuildMgr>();
-        //BuildMgr.Init();
-        //UIMgr = gameObject.AddComponent<UIMgr>();
-        //UIMgr.Init();
-    }
-
-    void Init()
-    {
-        
->>>>>>> 796e79390ecfda500864993fe55a5a4427d444d4
     }
 }
