957679a38ffb5e23f06a6c0c27e7ce134daaf42c
ETyoshino <576534340@qq.com>
2020-11-15 18:12:16 +0800

    1

---
+++ Assets/Scripts/Mgr/BuildMgr.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildMgr : MonoBehaviour
+{
+    public GameObject SelectObj;
+
+    private Vector3 BuildPlaneSize;
+    private Vector3 targetPos;
+    
+    [SerializeField]
+    private bool isBuild=false;
+    private bool canBuild;
+    private GameObject IsBuildPlane;
+
+    public void Init()
+    {
+        IsBuildPlane = transform.Find("HousePlane").gameObject;
+    }
+    
+    private void Update()
+    {
+        if (isBuild)
+        {
+            SelectPos();
+            
+            if (Input.GetMouseButtonDown(0)) SetStruct();
+        }
+    }
+
+    private void SelectPos()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane floor = new Plane(Vector3.up, Vector3.forward);
+        float distToGround=0;
+        if (floor.Raycast(ray,out distToGround))
+        {
+            Vector3 point = ray.GetPoint(distToGround);
+            SelectObj.transform.position = point;
+        }
+    }
+
+    public void OnSelectBuildStruct(GameObject _struct)
+    {
+        SelectObj = Instantiate(_struct);
+        BoxCollider col = SelectObj.GetComponent<BoxCollider>();
+        HouseTrigger _ht = SelectObj.AddComponent<HouseTrigger>();
+        
+        BuildPlaneSize = new Vector3(col.size.x,col.size.z,1);
+        targetPos = col.center;
+        targetPos.y = 0.1f;
+        IsBuildPlane.transform.SetParent(SelectObj.transform);
+        IsBuildPlane.transform.localPosition = targetPos;
+        IsBuildPlane.transform.localScale = BuildPlaneSize;
+        isBuild = true;
+    }
+
+    public void SetStruct()
+    {
+        
+    }
+}
--- Assets/Scripts/Mgr/GameMgr.cs
+++ Assets/Scripts/Mgr/GameMgr.cs
@@ -21,6 +21,19 @@ public class GameMgr : MonoBehaviour
     private void Awake()
     {
         Init();
+<<<<<<< HEAD
+=======
+        TimeMgr = gameObject.AddComponent<TimeMgr>();
+        TimeMgr.Inst();
+        HourseMgr = gameObject.AddComponent<HourseMgr>();
+        HourseMgr.Inst();
+        ConfigMgr = gameObject.AddComponent<ConfigMgr>();
+        ConfigMgr.Inst();
+        BuildMgr = gameObject.AddComponent<BuildMgr>();
+        //BuildMgr.Init();
+        UIMgr = gameObject.AddComponent<UIMgr>();
+        //UIMgr.Init();
+>>>>>>> parent of acc7fed0 (1)
     }
 
     void Init()
