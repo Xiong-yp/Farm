1d968ac28fc141df4b91e67eff5f07e867ebf2cc
ETyoshino <576534340@qq.com>
2020-11-15 18:15:19 +0800

    1

--- Assets/Scripts/Mgr/GameMgr.cs
+++ Assets/Scripts/Mgr/GameMgr.cs
@@ -12,7 +12,26 @@ public class GameMgr : MonoBehaviour
         get => _instance;
     }
 
+<<<<<<< HEAD
     public UIMgr UiMgr
+=======
+    public UIMgr UIMgr
+    {
+        get;
+        private set;
+    }
+    public ConfigMgr ConfigMgr
+    {
+        get;
+        private set;
+    } 
+    public TimeMgr TimeMgr
+    {
+        get;
+        private set;
+    }
+    public HourseMgr HourseMgr
+>>>>>>> parent of da34b860 (1)
     {
         get;
         private set;
@@ -22,18 +41,26 @@ public class GameMgr : MonoBehaviour
     {
         Init();
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         TimeMgr = gameObject.AddComponent<TimeMgr>();
+=======
+        /*TimeMgr = gameObject.AddComponent<TimeMgr>();
+>>>>>>> parent of da34b860 (1)
         TimeMgr.Inst();
         HourseMgr = gameObject.AddComponent<HourseMgr>();
         HourseMgr.Inst();
         ConfigMgr = gameObject.AddComponent<ConfigMgr>();
-        ConfigMgr.Inst();
+        ConfigMgr.Inst();*/
         BuildMgr = gameObject.AddComponent<BuildMgr>();
-        //BuildMgr.Init();
+        BuildMgr.Init();
         UIMgr = gameObject.AddComponent<UIMgr>();
+<<<<<<< HEAD
         //UIMgr.Init();
 >>>>>>> parent of acc7fed0 (1)
+=======
+        UIMgr.Init();
+>>>>>>> parent of da34b860 (1)
     }
 
     void Init()
